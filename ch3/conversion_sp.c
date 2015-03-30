26997a13f37ba633cfe36fc0891d6fc9bbec6b97
JIElite <ita3051@gmail.com>
2015-03-30 21:08:25 +0800

    let i_num converse to double, it work

--- ch3/conversion_sp.c
+++ ch3/conversion_sp.c
@@ -3,18 +3,19 @@
 int main(){ 
 
 	double d_num = 1024342.34223;
-	int i_num = 344954334;
+	int i_num = 100;
 	
 
 	printf("d_num : %g\n", d_num);
 	
 	/* 
-	 *	conversion_sp.c:12:25: warning: format specifies type 'double' but the argument has type 'int' [-Wformat]
+	 *	conversion_sp.c:17:25: warning: format specifies type 'double' but the argument has type 'int' [-Wformat]
          *		printf("i_num : %g\n", i_num);
          *        		        ~~     ^~~~~
          *               		%d 
 	 */
 	printf("i_num : %g\n", i_num);
+	printf("i_num : %g\n", (double)i_num);
 
 
 	return 0;
