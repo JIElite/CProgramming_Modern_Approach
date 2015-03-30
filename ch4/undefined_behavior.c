98abc8f4574d8d3eef6703d6a52ae847159e2b70
JIElite <ita3051@gmail.com>
2015-03-30 21:28:50 +0800

    finish undefine_behaviour.c

---
+++ ch4/undefined_behavior.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){ 
+	int i = 2;
+	int j = i * (i++);
+	int k = i * ++i;
+
+	
+	/* i think i = 4, j = 4, k = 12 */
+	printf("i : %d\n"
+	       "j : %d\n"
+	       "k : %d\n", i, j, k);
+	
+	/*
+	 * compiler :
+	 * 	test.c:5:15: warning: unsequenced modification and access to 'i' [-Wunsequenced]
+         * 		int j = i * i++;
+         *       		~    ^
+	 *	test.c:6:14: warning: unsequenced modification and access to 'i' [-Wunsequenced]
+         *		int k = i * ++i;
+         * 	        	~   ^
+	 */
+
+
+	return 0;
+
+}
