3db6bb60b5fab2dc3eff24c0cb854ec997d1e0c2
JIElite <ita3051@gmail.com>
2015-04-01 22:20:00 +0800

    add array_parameter.c

---
+++ ch9_function/array_parameter.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void test_func(int arr[3]){ 
+
+
+}
+
+int main(){ 
+
+	int array[10] = {  1, 3, 5 };
+
+	int *array2 = malloc(sizeof(int) * 25);
+	test_func(array2);
+
+}
